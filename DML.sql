5b068fb2ccea22f1a8e90cfc68defb809331010f
marciogui99 <56273108+marciogui99@users.noreply.github.com>
2021-11-24 11:28:44 -0300

    Add files via upload

--- DML.sql
+++ DML.sql
@@ -1,38 +1,53 @@
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (1, 'Caio Carlos', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (2, 'Luiz Luz', '2007-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (3, 'Marcelo Matos', '2004-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (4, 'Patricia Pereira', '2002-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (5, 'Maria Lucia', '2001-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (6, 'Jorge Menezes', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (7, 'Luiz Macedo', '2007-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (8, 'Marcelo Para', '2004-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (10, 'Joao Marcelo Paiva', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (11, 'Marciana Torres', '1993-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (11124, 'Juliana Meira', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (23, 'Luiza Souza', '1990-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (34, 'Rui Costa', '2000-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (101, 'Joao Doria', '2012-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (111, 'Tarcisio Pimenta', '2003-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (12, 'Wellington Dias', '1992-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (27, 'Raphael Novaes', '1950-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (9, 'Marcelo Camelo', '1950-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (104, 'Joao Jorge da Silva', '1953-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (1165, 'Gabriel Costa', '1960-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (145, 'Caio Maria', '1948-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (256, 'Luiza Pereira', '1922-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (376, 'Carla Zambeli', '1960-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (1086, 'Eduardo Paes', '2002-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (11987, 'Lucas Moura', '2001-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (176, 'Yan Almeida', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (2234, 'Rubens Alves', '2001-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (323423, 'Marcelo Moraes', '2004-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (10234, 'Joaquim Pereira', '2002-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (11234, 'Daniel Carlos', '2001-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (13243, 'Severino Costa', '1994-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (25432, 'Rui Junior', '2007-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (34573, 'Paco Rabanne', '2004-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (10234325, 'Leticia Almeida', '2002-03-03');
-INSERT INTO individuo(documento, nome, dt_nascimento) VALUES (11436, 'Mario Luigi', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('1', 'Caio Carlos', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('2', 'Luiz Luz', '2007-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('3', 'Marcelo Matos', '2004-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('4', 'Patricia Pereira', '2002-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('5', 'Maria Lucia', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('6', 'Jorge Menezes', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('7', 'Luiz Macedo', '2007-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('8', 'Marcelo Para', '2004-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('10', 'Joao Marcelo Paiva', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('11', 'Marciana Torres', '1993-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('11124', 'Juliana Meira', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('23', 'Luiza Souza', '1990-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('34', 'Rui Costa', '2000-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('101', 'Joao Doria', '2012-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('111', 'Tarcisio Pimenta', '2003-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('12', 'Wellington Dias', '1992-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('27', 'Raphael Novaes', '1950-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('9', 'Marcelo Camelo', '1950-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('104', 'Joao Jorge da Silva', '1953-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('1165', 'Gabriel Costa', '1960-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('145', 'Caio Maria', '1948-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('256', 'Luiza Pereira', '1922-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('376', 'Carla Zambeli', '1960-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('1086', 'Eduardo Paes', '2002-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('11987', 'Lucas Moura', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('176', 'Yan Almeida', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('2234', 'Rubens Alves', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('323423', 'Marcelo Moraes', '2004-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('10234', 'Joaquim Pereira', '2002-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('11234', 'Daniel Carlos', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('13243', 'Severino Costa', '1994-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('25432', 'Rui Junior', '2007-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('34573', 'Paco Rabanne', '2004-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('10234325', 'Leticia Almeida', '2002-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('11436', 'Mario Luigi', '2001-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('0589', 'Luigi Mario', '1960-03-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('453564', 'Marcio Guilherme', '1999-02-03');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('786787', 'Marcos Vieira', '1999-03-02');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('907988', 'Robson Silva', '1980-03-02');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('35345645', 'Eduardo Cesar', '1970-03-02');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('456578', 'Igor Loredo', '1980-09-09'); //
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('896786', 'Guilherme Machado', '1980-08-08');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('345767', 'Tatiane Ribeiro', '1980-07-07');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('43546', 'Maria Aparecida', '1980-04-04');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('7868', 'Aparecida de Souza', '1990-03-05');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('2356778', 'Tiago Fernandes', '2000-10-10');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('24536566', 'Cristiano Ronaldo', '1990-12-12');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('9096875', 'Joao Soares', '1995-08-02');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('98756567', 'Felipe Danelon', '1988-03-10');
+INSERT INTO individuo(documento, nome, dt_nascimento) VALUES ('665456', 'Leticia Almeida', '1988-08-08');
 SELECT * FROM individuo;
 
 
@@ -42,12 +57,22 @@ INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('376', '
 INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('11', 'Culpado(a)', '2009-03-12');
 INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('145', 'Culpado(a)', '1994-03-21');
 INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('104', 'Culpado(a)', '2014-03-04');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('9096875', 'Culpado(a)', '2018-06-06');//
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('896786', 'Culpado(a)', '2000-10-10');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('786787', 'Culpado(a)', '2020-05-04');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('907988', 'Culpado(a)', '2003-09-09');
+
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('23', 'Inocente', '2018-11-11');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('12', 'Inocente', '2019-12-12');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('23', 'Inocente', '2013-06-08');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('12', 'Inocente', '2011-03-04');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('8', 'Inocente', '2010-03-04');
 INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('23', 'Inocente', '2014-03-04');
-INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('12', 'Inocente', '2014-03-04');
-INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('23', 'Inocente', '2014-03-04');
-INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('12', 'Inocente', '2014-03-04');
-INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('8', 'Inocente', '2014-03-04');
-INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('23', 'Inocente', '2014-03-04');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('13243', 'Inocente', '2018-05-05');//
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('456578', 'Inocente', '2005-08-04');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('43546', 'Inocente', '2007-05-08');
+INSERT INTO processo_judicial(doc_reu, procedencia, dt_termino) VALUES ('9096875', 'Inocente', '2014-05-05');
+
 INSERT INTO processo_judicial(doc_reu) VALUES ('9');
 INSERT INTO processo_judicial(doc_reu) VALUES ('2');
 INSERT INTO processo_judicial(doc_reu) VALUES ('10234325');
@@ -56,6 +81,8 @@ INSERT INTO processo_judicial(doc_reu) VALUES ('256');
 INSERT INTO processo_judicial(doc_reu) VALUES ('176');
 INSERT INTO processo_judicial(doc_reu) VALUES ('2234');
 INSERT INTO processo_judicial(doc_reu) VALUES ('2');
+INSERT INTO processo_judicial(doc_reu) VALUES ('665456');
+INSERT INTO processo_judicial(doc_reu) VALUES ('10234325');
 -- TODO Criar rotina para impedir que processos sem data de termino sejam culpados
 SELECT * FROM processo_judicial;
 
@@ -64,6 +91,7 @@ INSERT INTO partido VALUES ('PSC', 'Partido Cristao');
 INSERT INTO partido VALUES ('PSDB', 'Tucanos');
 INSERT INTO partido VALUES ('PSL', 'Tradicao e aventura');  -- Pode string vazia
 INSERT INTO partido VALUES ('PSM', 'O maior dos maiores');
+INSERT INTO partido VALUES ('PDT', 'O maior dos maiores');
 SELECT * FROM partido;
 
 INSERT INTO cargo(nome,cidade,uf,federacao,qtd_eleitos) VALUES ('Prefeito', 'Belo Horizonte', 'MG', 'BR', 1);
